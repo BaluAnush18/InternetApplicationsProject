be7bb55823cb44cebe1d2b5d402ab67d67b94f30
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2024-07-25 22:13:53 -0400

    final url and style commit

--- ec/ec/urls.py
+++ ec/ec/urls.py
@@ -45,9 +45,12 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html',form_class=MySetPasswordForm) , name='password_reset_confirm'),
     path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html') , name='password_reset_complete'),
 
-    
+    path('user-history/', views.user_history, name='user_history'),
     
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Go Green"
 admin.site.site_title = "Go Green"
